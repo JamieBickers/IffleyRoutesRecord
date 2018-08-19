9323b2937f3d7ce5c47726b579e7d13d40394f5b
jmab <bickersjamie@googlemail.com>
2018-08-19 15:25:47 +0100

    Initial commit.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Logic;
+
+namespace Iffley.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var holds = ClimbsAnalysis.getHoldsWithOccuranceRateOrderedAscending();
+            return View();
+        }
+    }
+}
